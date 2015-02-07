6f05874e9d598c197428bef5a28f5b24ce178356
mgalushka <q1w2e3r4TY1>
2015-02-07 20:58:32 +0000

    512m just to make sure

--- Server/restart.bash
+++ Server/restart.bash
@@ -13,7 +13,7 @@ kill -9 `ps aux | grep 'beergaragehack' | awk '{print $2}'`
 echo 'Starting server'
 
 CLASSPATH="./lib/jcommander-1.35.jar:./lib/json_simple-1.1.jar:./lib/scribe-1.3.5.jar:./target/dependency/*:./target/beergaragehack-1.0.jar"
-JAVA_MEM="-Xms256m -Xmx256m"
+JAVA_MEM="-Xms512m -Xmx512m"
 JAVA_GC="-verbose:gc"
 
 nohup java -cp ${CLASSPATH} ${JAVA_MEM} ${JAVA_GC} com.garagehack.service.Service > out.log 2>&1 &
