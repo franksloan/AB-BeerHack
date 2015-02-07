c5e5bb4080449f71e0657098d3100f70a42246de
mgalushka <q1w2e3r4TY1>
2015-02-07 21:11:00 +0000

    EOF2

--- Server/cache.bash
+++ Server/cache.bash
@@ -9,3 +9,4 @@ curl -i -G http://54.148.62.37:8081/?name=Stella%20Artois%20Cidre%20Apple&locati
 curl -i -G http://54.148.62.37:8081/?name=Hoegaarden&location=51.5033630,-0.1276250
 curl -i -G http://54.148.62.37:8081/?name=Jupiler&location=51.5033630,-0.1276250
 curl -i -G http://54.148.62.37:8081/?name=Shock%20Top&location=51.5033630,-0.1276250
+
