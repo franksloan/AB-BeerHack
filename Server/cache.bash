b7f5c83fbabfefe3cffeb95ac0e49fc6b8902417
mgalushka <q1w2e3r4TY1>
2015-02-07 19:40:32 +0000

    Caching

--- Server/cache.bash
+++ Server/cache.bash
@@ -1,27 +1,27 @@
 #!/bin/bash
-nohup curl http://54.148.62.37:8081/?name=Budweiser&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Stella%20Artois&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Corona%20Extra&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Leffe%20Blonde&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Leffe%20Brune&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Leffe%20Brown&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Stella%20Artois%20Cidre%20Apple&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Hoegaarden&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Jupiler&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Modelo%20Especial&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Negra%20Modelo&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Quilmes%20Crystal&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Brahma%20Chopp&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Skol&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Harbin%20Ice&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Hertog%20Jan%20Grand%20Prestige&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Shock%20Top&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Franziskaner&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Patagnoia%20Weisse&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Patagonia%20Amber%20Lager&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Patagonia%20Bohemia%20Lager&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Bohemia&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Alexander%20Keith%20IPA&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Goose%20Island%20312%20Urban%20Wheat&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Goose%20Island%20Sofie&location=51.5033630,-0.1276250 &
-nohup curl http://54.148.62.37:8081/?name=Goose%20Island%20Matilda&location=51.5033630,-0.1276250 &
+curl http://54.148.62.37:8081/?name=Budweiser&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Stella%20Artois&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Corona%20Extra&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Leffe%20Blonde&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Leffe%20Brune&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Leffe%20Brown&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Stella%20Artois%20Cidre%20Apple&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Hoegaarden&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Jupiler&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Modelo%20Especial&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Negra%20Modelo&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Quilmes%20Crystal&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Brahma%20Chopp&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Skol&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Harbin%20Ice&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Hertog%20Jan%20Grand%20Prestige&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Shock%20Top&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Franziskaner&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Patagnoia%20Weisse&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Patagonia%20Amber%20Lager&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Patagonia%20Bohemia%20Lager&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Bohemia&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Alexander%20Keith%20IPA&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Goose%20Island%20312%20Urban%20Wheat&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Goose%20Island%20Sofie&location=51.5033630,-0.1276250
+curl http://54.148.62.37:8081/?name=Goose%20Island%20Matilda&location=51.5033630,-0.1276250
